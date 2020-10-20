7f938fd94cb64dba2627c84695a2257a61ae4c3c
Roman <romaniv788@gmail.com>
2020-10-20 09:47:05 +0300

    add presentations

---
+++ linters/code example/jshint_/index.js
@@ -0,0 +1,17 @@
+switch (cond) {
+    case "one":
+      doSomething(); // JSHint will warn about missing 'break' here.
+    case "two":
+      doSomethingElse();
+}
+
+var foo = bar;
+
+let abc;
+
+const ab = () => {
+  console.log('dkdjj');
+  console.log('kdk');
+}
+
+ab();
