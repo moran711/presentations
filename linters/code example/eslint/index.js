e66571ebe73e1ae9c462ddaf9fd546374907eb8a
Roman <romaniv788@gmail.com>
2020-10-23 08:58:28 +0300

    add code to jslinters code example

--- linters/code example/eslint/index.js
+++ linters/code example/eslint/index.js
@@ -1,17 +1,65 @@
 switch (cond) {
-    case "one":
-      doSomething(); // JSHint will warn about missing 'break' here.
-    case "two":
-      doSomethingElse();
+  case "one":
+    doSomething(); // JSHint will warn about missing 'break' here.
+  case "two":
+    doSomethingElse();
 }
 
 var foo = bar;
 
 let abc;
 
-const ab = () => {
-  console.log('dkdjj');
-  console.log('kdk');
+const {a} = {
+a: 'ddj'
 }
+
+const ab = () => {
+console.log('dkdjj');
+console.log('kdk');
+}
+
+let x = (function () {
+"use strict";
+return {
+    y: 1
+};
+})();
+
+const String = "My String";
+
+const demo = {
+hasOwnProperty: 1
+};
+
+
+let x = {
+actual: 10,
+get y() {
+    "use strict";
+    return this.actual / 2;
+},
+set y() {
+    "use strict";
+    this.actual = val;
+}
+};
+
+var xv = 1,
+  yv = 10;
+
+xv++;
+yv--;
+
+let x = {
+default: 10
+};
+
+
+class Cat {
+
+}
+
+const a = "10",
+  b = 5 - -a;
 
 ab();
--- linters/code example/jshint_/index.js
+++ linters/code example/jshint_/index.js
@@ -9,9 +9,57 @@ var foo = bar;
 
 let abc;
 
+const {a} = {
+  a: 'ddj'
+}
+
 const ab = () => {
   console.log('dkdjj');
   console.log('kdk');
 }
+
+let x = (function () {
+  "use strict";
+  return {
+      y: 1
+  };
+})();
+
+const String = "My String";
+
+const demo = {
+  hasOwnProperty: 1
+};
+
+
+let x = {
+  actual: 10,
+  get y() {
+      "use strict";
+      return this.actual / 2;
+  },
+  set y() {
+      "use strict";
+      this.actual = val;
+  }
+};
+
+var xv = 1,
+    yv = 10;
+
+xv++;
+yv--;
+
+let x = {
+  default: 10
+};
+
+
+class Cat {
+
+}
+
+const a = "10",
+    b = 5 - -a;
 
 ab();
