c135a6945900926523af36de805cf46c34e84c43
Roman <romaniv788@gmail.com>
2020-11-23 09:43:20 +0200

    add webpack presentation

---
+++ webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './path/to/my/entry/file.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js'
+  }
+};
