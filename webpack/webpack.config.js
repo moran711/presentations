d8bbe043c1659d1cfa9601a7c86e2f00c73c5f22
Roman <romaniv788@gmail.com>
2020-11-23 14:03:59 +0200

    add sass

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,5 +6,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
